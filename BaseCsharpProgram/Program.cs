ff082c333bfb4328f3c1b928c94dfebfabb68120
Mashyyyy <jullian@artblock.cz>
2026-05-28 15:32:22 +0800

    Commit base solution and csproject

---
+++ BaseCsharpProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BaseCsharpProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
